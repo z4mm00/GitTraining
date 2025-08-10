1789387be9d64167d10e430e70ca538c449a0463
Z4MM00 <z4mm00@hotmail.com>
2025-08-10 15:47:48 -0500

    Implement basic new operations for tasks

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -4,4 +4,23 @@ public class TaskManager {
         System.out.println("Task Manager initializing...");
         // TODO: Add methods for create, read, update, delete tasks
     }
+    // Method to create a new task
+    public void createTask(String taskName) {
+        System.out.println("Creating task: " + taskName);
+    }
+
+    //Method to list all tasks
+    public void listTasks() {
+        System.out.println("Listing all tasks...");
+    }
+
+    // Method to update an existing task
+    public void updateTask(String taskName, String newDetails) {
+        System.out.println("Updating task: " + taskName + " with new details: " + newDetails);
+    }
+
+    // Method to delete a task
+    public void deleteTask(String taskName) {
+        System.out.println("Deleting task: " + taskName);
+    }
 }
