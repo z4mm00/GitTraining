3af2e7ace9bfa9a39b2827a8645c582a3091f091
Z4MM00 <z4mm00@hotmail.com>
2025-08-10 15:30:51 -0500

    Add Task manager skeleton with main method

---
+++ src/TaskManager.java
@@ -0,0 +1,7 @@
+public class TaskManager {
+
+    public static void main(String[] args) {
+        System.out.println("Task Manager initializing...");
+        // TODO: Add methods for create, read, update, delete tasks
+    }
+}
